a291432c74a459eeedf76f44946d01ade569c908
Bradley S <bradleysantiago4@gmail.com>
2025-02-05 01:21:36 -0500

    first push

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,45 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    const token = this.getToken();
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    return decodedToken;
+  }
+
+  loggedIn() {
+    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
+  }
+  
+  isTokenExpired(token: string) {
+    // TODO: return a value that indicates if the token is expired
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp ? currentTime > decodedToken.exp : false;
+  }
+
+  getToken(): string {
+    // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  }
+
+  login(idToken: string) {
+    // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+    // TODO: redirect to the home page
+    window.location.assign('/');
+  }
+
+  logout() {
+    // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
+    // TODO: redirect to the login page
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
